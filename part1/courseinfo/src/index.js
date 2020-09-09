02a0a2cbd33e74244cfec54e864df291929af6d4
Bhavin <bhavinkapadia89@gmail.com>
2020-09-08 23:07:11 -0400

    remove div

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,35 +3,35 @@ import ReactDOM from 'react-dom'
 
 const Header = (props) => {
   return(
-    <div>
+    <>
       <h1> {props.course} </h1>
-    </div>
+    </>
   )
 }
 
 const Part = (props) => {
   return(
-    <div>
+    <>
       <p> {props.part} {props.exercise}</p>
-    </div>
+    </>
   )
 }
 
 const Content = (props) => {
   return(
-    <div>
+    <>
       <Part part={props.names[0].part} exercise={props.names[0].exercises} />
       <Part part={props.names[1].part} exercise={props.names[1].exercises} />
       <Part part={props.names[2].part} exercise={props.names[2].exercises} />
-    </div>
+    </>
   )
 }
 
 const Total = (props) => {
   return(
-    <div>
+    <>
         <p> Number of exercises {props.totals[0].exercises + props.totals[1].exercises + props.totals[2].exercises} </p>
-    </div>
+    </>
   )
 }
 const App = () => {
@@ -54,11 +54,11 @@ const App = () => {
   }
 
   return (
-    <div>
+    <>
       <Header course={course.name}/>
       <Content names={course.parts} />
       <Total totals={course.parts}/>
-    </div>
+    </>
   )
 }
 
