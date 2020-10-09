e88d4d78709e66d7daf542553ab2b46d30eceede
Bhavin <bhavinkapadia89@gmail.com>
2020-10-08 22:27:38 -0400

    update folder structure

---
+++ part1_workbook/part1d/src/index.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+// const History = (props) => {
+//   console.log(props)
+//   if (props.allClicks.lenght === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+// const Button = ({ onClick, text }) => (
+//   <button onClick={onClick}>
+//     {text}
+//   </button>
+// )
+
+
+
+// const App = (props) => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+  // const [clicks, setClicks] = useState({
+  //   left:0, 
+  //   right:0
+  // })
+
+  // const handleLeftClick = () => {
+  //   setClicks({
+  //     ...clicks,
+  //     left: clicks.left+1,
+  //   })
+  // }
+
+  // const handleRightClick = () => {
+  //   setClicks({
+  //     ...clicks,
+  //     right: clicks.right + 1
+  //   })
+  // }
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left+1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right+1)
+//   } 
+
+//   return (
+//     <div>
+//       <div>
+//         {left}
+//         <Button onClick={handleLeftClick} text='left' />
+//         <Button onClick={handleRightClick} text='right' />
+//         {right}
+//         <History allClicks ={allClicks} />
+//       </div>
+//     </div>
+//   )
+// }
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = props => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root'))
+  
